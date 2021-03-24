cef6dd3d4f79fd2da5fb84b75b498b05b3f4a116
kimtajung1 <kimtajung1@puzzledata.com>
2021-03-24 15:31:05 +0900

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import { Route } from 'react-router-dom';
+
+import Main from "./Main";
+import Test2 from "./Test2";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route exact path="/" component={Main} />
+      <Route path="/Test2" component={Test2} />
     </div>
   );
 }
---
+++ src/Test2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Test2() {
+  return (
+    <div className="App">
+      <h1>TEST2</h1>
+    </div>
+  );
+}
+
+export default Test2;
