3e86f939eb61a51f8e88484dcf8117731c691210
kimtajung1 <kimtajung1@puzzledata.com>
2021-04-05 16:01:44 +0900

    1. 저장버튼 생성 2.라벨링 진행중

--- src/components/Write/Write.tsx
+++ src/components/Write/Write.tsx
@@ -1,15 +1,64 @@
-import React, { useState, useRef } from 'react';
+import React, { useState} from 'react';
 import './Write.css';
+import { Button } from "@chakra-ui/react"
+import { Badge } from "@chakra-ui/react"
 
 import { firestore } from "../../firebase.js";
 import { v4 as uuidv4 } from 'uuid';
 
-function Write() {
-  const [title, setTitle] = useState<string>("제목");
-  const [text, setText] = useState<string>("내용");
-  const [label, setLabel] = useState<[]>([]);
-  const inputEl = useRef(null);
+const labelColors = ["blue", "cyan", "gray", "green", "orange", "pink", "purple", "red", "teal", "yellow", "whiteAlpha", "blackAlpha", "linkedin", "facebook", "messenger", "whatsapp", "twitter", "telegram"];
 
+function Write() {
+  let [title, setTitle] = useState<string>("");
+  let [text, setText] = useState<string | null>("");
+  let [labels, setLabels] = useState<any>([]);
+  let [curLabel, setCurLabel] = useState<any>("");
+  let [labelTheme, setLabelTheme] = useState<any>([]);
+
+  const changeLabel = (e: any) => {    
+    const { name, value } = e.target
+    let labelInfo = {
+      theme: "",
+      value: value
+    }
+    setCurLabel(labelInfo);        
+  }
+
+  const addLabel = async (e: any) => {      
+    if(e.key === "Enter"){
+
+      let randomIdx = Math.floor(Math.random() * 18);
+      let newTheme = labelColors[randomIdx];
+      let oldLabels = [];
+
+      if (!labelTheme.includes(newTheme)){        
+        await setLabelTheme([...labelTheme, newTheme]);
+        await setCurLabel({theme: newTheme});
+      }
+      await (()=> {
+        oldLabels = [...labels];
+        oldLabels.push(curLabel);
+      })();
+
+      await setLabels(oldLabels);
+      await setCurLabel(curLabel = {
+        theme: "",
+        value: ""
+      });
+            
+    }    
+  }
+
+  const renderLabel = () => {
+    console.log(labels)
+    return labels.map(
+      (label: any, idx: number) => {
+        return <Badge colorScheme={label.theme} key={`label-${idx}`} fontSize="1em">{label.value}</Badge>;
+      }
+    )
+  }
+
+  
   return (
     <div id="write">
       
@@ -17,14 +66,19 @@ function Write() {
         <div className="write-content">
           <div className="write-content-border">
             <div className="write-title">
-              <input placeholder="제목" onChange={e => setTitle(e.target.value)}/>
+              <input className="input-border" placeholder="제목" onChange={(e) => setTitle(e.target.value)}/>
             </div>
             <div className="write-text">
-              <div className="textarea" contentEditable="true"placeholder="내용" onInput={e => console.log(e.currentTarget.textContent)}/>
+              <div className="textarea input-border" contentEditable="true" placeholder="내용" onInput={(e) => setText(e.currentTarget.textContent)}/>
             </div>
-            <div className="write-label">
-              <button onClick = {() => console.log("title : ", title)}>제목 테스트 </button>
-              <button onClick = {() => console.log("text : ", text)}>내용 테스트 </button>
+            <div className="write-footer">
+              <input className="input-border" value={curLabel.value} onChange={changeLabel} onKeyPress={(e) => addLabel(e)} placeholder="#을 사용해 라벨 생성"/>
+              <div className="labels-wrapper">
+                { renderLabel() }
+              </div>
+              <div className="btn-wrapper">
+                <Button colorScheme="gray" variant="outline">저장</Button>
+              </div>              
             </div>
           </div>
         </div>
